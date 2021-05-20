178af4dd17b591265f4c5c0918a91f821cb92e91
Rajat Khurana <rajatkhuranak@gmail.com>
2021-05-20 16:20:19 +0530

    added routing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import logo from "./logo.svg";
 import { Typography } from "@material-ui/core";
+import { BrowserRouter, Switch, Route, Link } from "react-router-dom";
+import About from "./component/About";
 import "./App.css";
 
 function App() {
@@ -8,14 +10,28 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <Typography variant="h1">Always visible</Typography>
-        <a
+        <BrowserRouter>
+          <Switch>
+            <Route path="/about">
+              <About />
+            </Route>
+            {/* <Route path="/users">
+            <Users />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route> */}
+          </Switch>
+        </BrowserRouter>
+
+        {/* <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a> */}
       </header>
     </div>
   );
